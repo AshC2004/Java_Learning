7bd45c3d30fcb29a132e308f6f15cb792941da79
AshC2004 <asusbackyp@gmail.com>
2025-07-17 16:00:33 +0530

    Variables in Java

---
+++ src/Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 21;
+
+        // Double (decimal number)
+        double salary = 1000000.00;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean (true/false)
+        boolean istrue = true;
+
+        // String (text)
+        String name = "Ashish";
+
+        // Output eg
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: Rs." + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java a Object-Oriented programming language? " + istrue);
+        //Java is based on OOP principles, everything is made using objects and classes.
+    }
+}
